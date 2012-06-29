2127ff7e8107058eed315b31a3da4555ced1feab
Nicolas Rannou <huko.rannou@gmail.com>
2012-06-29 16:12:41 -0400

    wip: template of welcome page

--- frontend/index.php
+++ frontend/index.php
@@ -4,23 +4,19 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Bootstrap, from Twitter</title>
+    <title>ChRIS Reloaded</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    
+    <meta name="apple-mobile-web-app-capable" content="yes">
 
-    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- Le styles -->
+    <!-- style -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-    <!--<link href="assets/css/docs.css" rel="stylesheet">-->
     <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
 
-    <!-- Le fav and touch icons -->
+    <!-- fav and touch icons -->
     <link rel="shortcut icon" href="assets/ico/favicon.ico">
     <link rel="apple-touch-icon" href="assets/ico/apple-touch-icon.png">
     <link rel="apple-touch-icon" sizes="72x72" href="assets/ico/apple-touch-icon-72x72.png">
@@ -29,167 +25,85 @@
   
   <style>
   	h1 {
-  		margin: 20px 0;
+  		margin-top: 50px;
+  		margin-bottom: 20px;
   	}
   	
   	.mainsection {
   		margin-bottom: 20px;
   	}
-  </style>
+  	</style>
+  
+  	<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <a class="brand" href="#">ChRIS Reloaded</a>
+        <div class="nav-collapse">
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Patient</a></li>
+            <li><a href="#">Pipeline</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">PACS</a></li>
+          </ul>
+          <form class="navbar-search pull-left" action="">
+            <input type="text" class="search-query span4" placeholder="Command">
+          </form>
+          <ul class="nav pull-right">
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Login</a></li>
+            </li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div>
+    </div><!-- /navbar-inner -->
+  </div><!-- /navbar -->
+    
 
+        </div>
+      </div>
+    </div>
   <body data-spy="scroll" data-target=".subnav" data-offset="50">
+  	
+
+    
 
 	<div class="container">
 		
 		<div class="row">
 			<div class="span12">
-				<h1>Bootstrap 2 Demo</h1>
+				<h1>ChRIS Reloaded</h1>
 			</div>
 		</div>
 		
-		<div class="row">
-			<div class="span12" style="margin-bottom: 20px;">
-				<div class="btn-group">
-				  <a class="btn" href="#">One</a>
-				  <a class="btn" href="#">Two</a>
-				  <a class="btn" href="#">Three</a>
-				  <a class="btn" href="#">Four</a>
-				  <a class="btn" href="#">Five</a>
-				  <a class="btn" href="#">Six</a>
-				</div>
-			</div>
-		</div>
-		
-		<div class="row">
-			<div class="span6">
-				<div class="progress">
-				  <div class="bar"
-				       style="width: 20%;"></div>
-				</div>
-			</div>
-			
-			<div class="span6">
-				<div class="progress progress-danger progress-striped active">
-				  <div class="bar"
-				       style="width: 60%;"></div>
-				</div>
-			</div>
-		</div>
-		
-		<div class="row">
-			<div class="span6">
-				<div class="progress">
-				  <div class="bar"
-				       style="width: 40%;"></div>
-				</div>
-			</div>
-			
-			<div class="span6">
-				<div class="progress progress-danger progress-striped active">
-				  <div class="bar"
-				       style="width: 40%;"></div>
-				</div>
-			</div>
-		</div>
-		
-		<div class="row">
-			<div class="span6">
-				<div class="progress">
-				  <div class="bar"
-				       style="width: 60%;"></div>
-				</div>
-			</div>
-			
-			<div class="span6">
-				<div class="progress progress-danger progress-striped active">
-				  <div class="bar"
-				       style="width: 20%;"></div>
-				</div>
-			</div>
-		</div>
-		
-		<div class="row">
-			<div class="span12">
-				<ul class="thumbnails">
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				  
-				  <li class="span3">
-				    <a href="#" class="thumbnail">
-				      <img src="http://placehold.it/260x180" alt="">
-				    </a>
-				  </li>
-				</ul>
-			</div>
-		</div>
 		
 		<div class="row mainsection">
-		  <div class="span4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur. Fusce nec leo ut massa viverra venenatis.</p></div>
-		  <div class="span4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur. Fusce nec leo ut massa viverra venenatis.</p></div>
-		  <div class="span4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur. Fusce nec leo ut massa viverra venenatis.</p></div>
-		</div>
-		
-		<div class="row">
-			<div class="span8">
-				<div id="myCarousel" class="carousel slide">
+		  <div class="span8"><p>Welcome Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur. Fusce nec leo ut massa viverra venenatis.</p>
+		  	                 <div id="myCarousel" class="carousel slide">
 				  <div class="carousel-inner">
 				    <div class="item active">
-				      <img src="assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
+				      <img src="assets/img/carina.jpg" alt="">
 				      <div class="carousel-caption">
-				        <h4>First Thumbnail label</h4>
+				        <h4>First Pipeline</h4>
 				        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				      </div>
 				    </div>
 				    <div class="item">
-				      <img src="assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
+				      <img src="assets/img/echo.jpg" alt="">
 				      <div class="carousel-caption">
-				        <h4>Second Thumbnail label</h4>
+				        <h4>Second Pipeline</h4>
 				        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				      </div>
 				    </div>
 				    <div class="item">
-				      <img src="assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
+				      <img src="assets/img/ngc5866.jpg" alt="">
 				      <div class="carousel-caption">
-				        <h4>Third Thumbnail label</h4>
+				        <h4>Third Pipeline</h4>
 				        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
 				      </div>
 				    </div>
@@ -197,12 +111,29 @@
 				  <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
 				  <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
 				</div>
-			</div>
-			
-			<div class="span4">
-				<h2>Slideshow</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod ultrices ante, ac laoreet nulla vestibulum adipiscing. Nam quis justo in augue auctor imperdiet. Curabitur aliquet orci sit amet est posuere consectetur. Fusce nec leo ut massa viverra venenatis. Nam accumsan libero a elit aliquet quis ullamcorper arcu tincidunt. Praesent purus turpis, consectetur quis congue vel, pulvinar at lorem. Vivamus varius condimentum dolor, quis ultricies ipsum porta quis. </p>
-				
+		  	                 
+		  	                 </div>
+		  <div class="span4">
+		  	<h3> ChRIS RSS Feed </h3>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		    <p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	<p>26-06-2012 Daniel logged in</p>
+		  	</div>
+		  </div>
+		  
+	    <div class="row">
+			<div class="span12">
+				Sponsors - FNNDSC- CHB
 			</div>
 		</div>
 		
