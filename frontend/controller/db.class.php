c6f2b8ff44522b9a0396b08eed4529fa2c70e7f6
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-07-09 17:12:45 -0400

    Skeleton for the database class and its test.

---
+++ frontend/controller/db.class.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ *
+ *            sSSs   .S    S.    .S_sSSs     .S    sSSs
+ *           d%%SP  .SS    SS.  .SS~YS%%b   .SS   d%%SP
+ *          d%S'    S%S    S%S  S%S   `S%b  S%S  d%S'
+ *          S%S     S%S    S%S  S%S    S%S  S%S  S%|
+ *          S&S     S%S SSSS%S  S%S    d* S  S&S  S&S
+ *          S&S     S&S  SSS&S  S&S   .S* S  S&S  Y&Ss
+ *          S&S     S&S    S&S  S&S_sdSSS   S&S  `S&&S
+ *          S&S     S&S    S&S  S&S~YSY%b   S&S    `S*S
+ *          S*b     S*S    S*S  S*S   `S%b  S*S     l*S
+ *          S*S.    S*S    S*S  S*S    S%S  S*S    .S*P
+ *           SSSbs  S*S    S*S  S*S    S&S  S*S  sSS*S
+ *            YSSP  SSS    S*S  S*S    SSS  S*S  YSS'
+ *                         SP   SP          SP
+ *                         Y    Y           Y
+ *
+ *                     R  E  L  O  A  D  E  D
+ *
+ * (c) 2012 Fetal-Neonatal Neuroimaging & Developmental Science Center
+ *                   Boston Children's Hospital
+ *
+ *              http://childrenshospital.org/FNNDSC/
+ *                        dev@babyMRI.org
+ *
+ */
+
+// security check
+require_once('_security.inc.php');
+
+
+/**
+ *
+ * The database connector.
+ *
+ */
+class DB {
+
+  private static $instance = null;
+
+  private $link = null;
+
+  private function __construct() {
+
+    $link = new mysqli(SQL_HOST, SQL_USERNAME, SQL_PASSWORD, SQL_DATABASE);
+
+    if ($link->connect_errno) {
+
+      throw new Exception('Failed to connect to database: '.$link->connect_error);
+
+    }
+
+  }
+
+  public static function getInstance() {
+
+    if (!self::$instance) {
+
+      self::$instance = new DB();
+
+    }
+
+    return self::$instance;
+
+  }
+
+  public function execute($query) {
+
+    $this->link->query($query);
+
+  }
+
+}
+
+?>
