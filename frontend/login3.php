ca89c29bb51c94f3f9f180545245fcabd722f531
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-07-12 17:00:25 -0400

    Work in progress on a tiled front page.

---
+++ frontend/login3.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ *            sSSs   .S    S.    .S_sSSs     .S    sSSs
+ *           d%%SP  .SS    SS.  .SS~YS%%b   .SS   d%%SP
+ *          d%S'    S%S    S%S  S%S   `S%b  S%S  d%S'
+ *          S%S     S%S    S%S  S%S    S%S  S%S  S%|
+ *          S&S     S%S SSSS%S  S%S    d* S  S&S  S&S
+ *          S&S     S&S  SSS&S  S&S   .S* S  S&S  Y&Ss
+ *          S&S     S&S    S&S  S&S_sdSSS   S&S  `S&&S
+ *          S&S     S&S    S&S  S&S~YSY%b   S&S    `S*S
+ *          S*b     S*S    S*S  S*S   `S%b  S*S     l*S
+ *          S*S.    S*S    S*S  S*S    S%S  S*S    .S*P
+ *           SSSbs  S*S    S*S  S*S    S&S  S*S  sSS*S
+ *            YSSP  SSS    S*S  S*S    SSS  S*S  YSS'
+ *                         SP   SP          SP
+ *                         Y    Y           Y
+ *
+ *                     R  E  L  O  A  D  E  D
+ *
+ * (c) 2012 Fetal-Neonatal Neuroimaging & Developmental Science Center
+ *                   Boston Children's Hospital
+ *
+ *              http://childrenshospital.org/FNNDSC/
+ *                        dev@babyMRI.org
+ *
+ */
+
+// we define a valid entry point
+define('__CHRIS_ENTRY_POINT__', 666);
+
+//define('CHRIS_CONFIG_DEBUG', true);
+
+// include the configuration
+require_once ('config.inc.php');
+
+// include the template class
+require_once (joinPaths(CHRIS_CONTROLLER_FOLDER, 'template.class.php'));
+
+echo new Template('login4.html');
+
+?>
