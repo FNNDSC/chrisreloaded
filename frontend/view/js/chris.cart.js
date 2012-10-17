f1d38ba03cb2fa66884fb6feee421863e1cd740b
Nicolas Rannou <huko.rannou@gmail.com>
2012-10-17 11:16:55 -0400

    change cart location to the left hand side

--- frontend/view/js/chris.cart.js
+++ frontend/view/js/chris.cart.js
@@ -2,38 +2,24 @@
  * Define the CART namespace
  */
 var _CART_ = _CART_ || {};
-
-_CART_.show = function(x,y) {
+_CART_.show = function(x, y) {
   var hidden = jQuery('.cart').is(':hidden');
-  
   if (hidden) {
     jQuery('.cart').show();
-    // don't loose the current spot
-    
-    // TODO find good value.. tried adding jQuery('.cart').height() + 20 (for the margin without good success)
-    window.scrollTo(x,y+50);
-    
   }
 }
-
-_CART_.select = function(x,y) {
-  
-  _CART_.show(x,y);
-
+_CART_.select = function(x, y) {
+  _CART_.show(x, y);
   // show a notification which hides it self after a while
-  jQuery().toastmessage('showSuccessToast','<b>'+_CART_.SeriesDesc+'</b><br>selected.');
-  
-  jQuery('#cart_datalist').append('<li>'+_CART_.SeriesDesc+'</li>');
-  
+  jQuery().toastmessage('showSuccessToast',
+      '<b>' + _CART_.SeriesDesc + '</b><br>selected.');
+  jQuery('#cart_datalist').append('<li>' + _CART_.SeriesDesc + '</li>');
 };
-
 /**
  * Setup the javascript when document is ready (finshed loading)
  */
 jQuery(document).ready(function() {
-  
   jQuery('#pipelines').carousel({
-    interval: false
+    interval : false
   });
-  
 });
