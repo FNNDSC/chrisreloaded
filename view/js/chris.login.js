4514bbde6eebd4ce42ce84ff48ac9e863b895b9e
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2012-11-26 14:33:02 -0500

    Include javascript for the login page.

---
+++ view/js/chris.login.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(
+    function() {
+
+      // check if we should be sorry
+      if (location.href.match(/(\?)(\w*.\w*)*/)) {
+        
+        var _command = location.href.match(/(\?)(\w*.\w*)*/)[0];
+        _command = _command.replace('?', '').replace('/', ''); // replace any ?
+                                                                // or /
+        
+        if (_command == 'sorry') {
+          
+          jQuery().toastmessage('showErrorToast',
+              '<h5>Login failed.</h5>' + 'Invalid credentials!');
+          
+        } else {
+          
+          // some easteregg
+          alert('Hi ' + _command);
+          
+        }
+      }
+      
+    });
+
+// activate backstretch
+jQuery.backstretch('view/gfx/background1.jpg');
