defe2fa0cfbd80aeb8d8e4fb8b4c53c9c5ea5e64
Nicolas Rannou <huko.rannou@gmail.com>
2013-01-04 12:34:00 -0500

    introduce study model and tests in db

---
+++ model/study.model.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ *
+ *            sSSs   .S    S.    .S_sSSs     .S    sSSs
+ *           d%%SP  .SS    SS.  .SS~YS%%b   .SS   d%%SP
+ *          d%S'    S%S    S%S  S%S   `S%b  S%S  d%S'
+ *          S%S     S%S    S%S  S%S    S%S  S%S  S%|
+ *          S&S     S%S SSSS%S  S%S    d* S  S&S  S&S
+ *          S&S     S&S  SSS&S  S&S   .S* S  S&S  Y&Ss
+ *          S&S     S&S    S&S  S&S_sdSSS   S&S  `S&&S
+ *          S&S     S&S    S&S  S&S~YSY%b   S&S    `S*S
+ *          S*b     S*S    S*S  S*S   `S%b  S*S     l*S
+ *          S*S.    S*S    S*S  S*S    S%S  S*S    .S*P
+ *           SSSbs  S*S    S*S  S*S    S&S  S*S  sSS*S
+ *            YSSP  SSS    S*S  S*S    SSS  S*S  YSS'
+ *                         SP   SP          SP
+ *                         Y    Y           Y
+ *
+ *                     R  E  L  O  A  D  E  D
+ *
+ * (c) 2012 Fetal-Neonatal Neuroimaging & Developmental Science Center
+ *                   Boston Children's Hospital
+ *
+ *              http://childrenshospital.org/FNNDSC/
+ *                        dev@babyMRI.org
+ *
+ */
+
+// prevent direct calls
+if (!defined('__CHRIS_ENTRY_POINT__'))
+  die('Invalid access.');
+
+// grab the super class for all entities
+require_once 'object.model.php';
+
+/**
+ *
+ * The Study class which describes the Study entity of the database.
+ *
+ */
+class Study extends Object {
+
+  /**
+   * The study description
+   *
+   * @var string $description
+   */
+  public $description = '';
+
+  /**
+   * The study location.
+   *
+   * @var string $location
+   */
+  public $location = '';
+
+  /**
+   * The patient age at scan date
+   *
+   * @var int $age
+   */
+  public $age = -1;
+  
+  /**
+   * The modality in this study.
+   *
+   * @var string $modality
+   */
+  public $modality = '';
+
+  /**
+   * The study date.
+   *
+   * @var string $date
+   */
+  public $date = '';
+}
+?>
