687d6c6d66100a8cd728c2f50e86307827cc3e0c
Nicolas Rannou <huko.rannou@gmail.com>
2013-01-09 15:13:31 -0500

    first attempt to solve the join tables overwritting issue
    efficiency might be imporved later on
    
    Closes #38

--- controller/mapper.class.php
+++ controller/mapper.class.php
@@ -348,7 +348,7 @@ class Mapper implements MapperInterface {
 
     // query the database
     $results = DB::getInstance()->execute('SELECT * FROM '.strtolower($this->objectname).strtolower($this->joins).strtolower($this->_getWhere()).strtolower($this->group).strtolower($this->order), $this->_getParam());
-
+    
     // create an array to store the objects
     $objects = Array();
 
@@ -366,8 +366,10 @@ class Mapper implements MapperInterface {
 
         // parse on result
         foreach ($result as $field) {
+          // strip potential '-'
+          $clean = explode('-', $field[0]);
           // if we reach a "id" field, create new object
-          if ($field[0] == 'id') {
+          if ($clean[0] == 'id') {
             // if there is an object existing, push it to right location and update localid
             // we only push the object once it has been filled!
             if (!empty($object)) {
@@ -378,7 +380,7 @@ class Mapper implements MapperInterface {
             $object = new $this->objects[$localid]();
           }
           // update fields
-          $object->$field[0] = trim($field[1]);
+          $object->$clean[0] = trim($field[1]);
         }
         // push last object to the right location
         // we only push the object once it has been filled!
