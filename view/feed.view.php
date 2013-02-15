be291f058c18f4596b54a57f8c4c9da356c99d86
Nicolas Rannou <huko.rannou@gmail.com>
2013-02-15 09:45:27 -0500

    code cleanup

--- view/feed.view.php
+++ view/feed.view.php
@@ -82,20 +82,20 @@ class FeedV implements ObjectViewInterface {
     // create the status text
     $status_text = '<font color=red>Running <i class="icon-refresh rotating_class"></i></font>';
     // ('.$object->status.'%)
-    $share_text = '';
+    $share_icon = '';
     if ($object->status == 100) {
       $status_text = '<font color=green>Done</font>';
-      $share_text = '<i class="icon-share-alt"></i>';
+      $share_icon = 'icon-share-alt';
     }
 
-    $archive_text = '<i class="icon-remove"></i>';
+    $archive_icon = 'icon-remove';
     if ($object->archive == '1') {
-      $archive_text = '<i class="icon-plus"></i>';
+      $archive_icon = 'icon-plus';
     }
 
-    $favorite_text = '<i class="icon-star-empty"></i>';
+    $favorite_icon = 'icon-star-empty';
     if ($object->favorite == '1') {
-      $favorite_text = '<i class="icon-star"></i>';
+      $favorite_icon = 'icon-star';
     }
 
     $t = new Template('feed.html');
@@ -110,12 +110,11 @@ class FeedV implements ObjectViewInterface {
     $t -> replace('FEED_META_CONTENT', $feed_meta_advanced);
     $t -> replace('TIME_FORMATED', $object->time);
     $t -> replace('PLUGIN', ucwords(str_replace('_',' ',$object->plugin)));
-    $t -> replace('MORE', '<i class="icon-chevron-right"></i>');
     $t -> replace('STATUS', $object->status);
     $t -> replace('STATUS_TEXT', $status_text);
-    $t -> replace('SHARE_TEXT', $share_text);
-    $t -> replace('ARCHIVE_TEXT', $archive_text);
-    $t -> replace('FAVORITE_TEXT', $favorite_text);
+    $t -> replace('SHARE_ICON', $share_icon);
+    $t -> replace('ARCHIVE_ICON', $archive_icon);
+    $t -> replace('FAVORITE_ICON', $favorite_icon);
     // set data browser
     $d = new Template('feed_data_browser.html');
     $feed_folder = joinPaths($username,$object->plugin, $object->name.'-'.$object->id);
