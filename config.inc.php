a734ab45e5e98ec8db3036c9a24c48a252a5b998
Nicolas Rannou <huko.rannou@gmail.com>
2013-02-18 12:10:30 -0500

    add new plugin email address

--- config.inc.php
+++ config.inc.php
@@ -34,6 +34,7 @@ require_once(dirname(__FILE__).'/controller/_util.inc.php');
 
 // admin email
 define('CHRIS_ADMIN_EMAIL', 'chris@babymri.org');
+define('CHRIS_PLUGIN_EMAIL_FROM', 'plugin@chris.org');
 define('CHRIS_DICOM_EMAIL_FROM', 'dicom@chris.org');
 define('CHRIS_DICOM_EMAIL_TO', 'nicolas.rannou@childrens.harvard.edu,rudolph.pienaar@childrens.harvard.edu,borjan.gagoski@childrens.harvard.edu');
 
