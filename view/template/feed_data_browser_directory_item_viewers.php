1eae8f27b62e11d006de3aa6acf4cb5432e274a8
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2013-03-28 16:19:28 -0400

    Detect a Freesurfer folder as a first step to the SurfView integration.

--- view/template/feed_data_browser_directory_item_viewers.php
+++ view/template/feed_data_browser_directory_item_viewers.php
@@ -2,6 +2,11 @@
 
   $fullpath = $GLOBALS['fullpath'];
 
+
+  //
+  // check for dicom files
+  //
+
   $dicom_files = glob($fullpath."/{*.dcm,*.dicom}",GLOB_BRACE);
 
   if (count($dicom_files) > 0) {
@@ -13,6 +18,20 @@
 <img
 	src='view/gfx/slicedrop.png' title='Open with Slice:Drop'
 	class='slicedrop_icon focus' onclick='var e = arguments[0] || window.event; e.stopPropagation(); _FEED_.slicedrop_dicom(e);' style='position: absolute; right: 10px;'>
+<?php
+
+  }
+
+  //
+  // check for freesurfer folder
+  //
+
+  $freesurfer_meshes = glob($fullpath."/surf/{lh.*,rh.*}", GLOB_BRACE);
+
+  if (count($freesurfer_meshes) > 0) {
+
+?>
+<span style='position: absolute; right: 10px;'>SurfView</span>
 <?php
 
   }
