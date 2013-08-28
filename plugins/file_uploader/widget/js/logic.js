7521696c482b326af51e188f918d8a51dfde17a7
Nicolas Rannou <huko.rannou@gmail.com>
2013-08-28 11:41:13 -0400

    improve user experience

--- plugins/file_uploader/widget/js/logic.js
+++ plugins/file_uploader/widget/js/logic.js
@@ -14,18 +14,8 @@ _CHRIS_INTERACTIVE_PLUGIN_.init = function() {
   _CHRIS_INTERACTIVE_PLUGIN_.dropReady = document.getElementById("dropready");
   _CHRIS_INTERACTIVE_PLUGIN_.targetFeed = null
   _CHRIS_INTERACTIVE_PLUGIN_.count = 0;
-
-  // create the feed
-  _CHRIS_INTERACTIVE_PLUGIN_.force = true;
-  document.getElementById("plugin_submit").click();
-}
-
-_CHRIS_INTERACTIVE_PLUGIN_.submitted = function(val) {
-  // let's go! Feed has been created (update UI to let user know we are ready)
-  // we need to create the feed first to have a container to put the images
-  _CHRIS_INTERACTIVE_PLUGIN_.targetFeed = val.split("\n")[1];
-  _CHRIS_INTERACTIVE_PLUGIN_.dropLoad.style.display = "none";
-  _CHRIS_INTERACTIVE_PLUGIN_.dropReady.style.display = "block";
+  _CHRIS_INTERACTIVE_PLUGIN_.countStyle = 0;
+  _CHRIS_INTERACTIVE_PLUGIN_.targetFiles = null;
 
   // attach events
   _CHRIS_INTERACTIVE_PLUGIN_.dropMaskArea.addEventListener("dragenter",_CHRIS_INTERACTIVE_PLUGIN_.onDragEnter, false);
@@ -34,6 +24,16 @@ _CHRIS_INTERACTIVE_PLUGIN_.submitted = function(val) {
   _CHRIS_INTERACTIVE_PLUGIN_.dropMaskArea.addEventListener("dragover",_CHRIS_INTERACTIVE_PLUGIN_.onDragOver, false);
   _CHRIS_INTERACTIVE_PLUGIN_.dropMaskArea.addEventListener("drop", _CHRIS_INTERACTIVE_PLUGIN_.onDrop, false);
   _CHRIS_INTERACTIVE_PLUGIN_.dropMaskArea.addEventListener("dragend", _CHRIS_INTERACTIVE_PLUGIN_.onDrop, false);
+
+}
+
+_CHRIS_INTERACTIVE_PLUGIN_.submitted = function(val) {
+  // let's go! Feed has been created (update UI to let user know we are ready)
+  // we need to create the feed first to have a container to put the images
+  _CHRIS_INTERACTIVE_PLUGIN_.targetFeed = val.split("\n")[1];
+
+  _CHRIS_INTERACTIVE_PLUGIN_.handleFiles(_CHRIS_INTERACTIVE_PLUGIN_.targetFiles);
+
 }
 
 // important in chrome, if not drag and drop do NOT work
@@ -61,11 +61,23 @@ _CHRIS_INTERACTIVE_PLUGIN_.onDrop = function (e) {
   e.preventDefault();
   // reset style
   _CHRIS_INTERACTIVE_PLUGIN_.dropArea.style.border = "1px solid #8D8D8D";
+
+  if(_CHRIS_INTERACTIVE_PLUGIN_.count == 0){
+    // store reference to files
+    _CHRIS_INTERACTIVE_PLUGIN_.targetFiles = e.dataTransfer.files;
+    // style output (for user interaction)
+    _CHRIS_INTERACTIVE_PLUGIN_.handleStyle(e.dataTransfer.files);
+    // create the feed
+    _CHRIS_INTERACTIVE_PLUGIN_.force = true;
+    document.getElementById("plugin_submit").click();
+    return;
+  }
   // do sth with those files!
+  _CHRIS_INTERACTIVE_PLUGIN_.handleStyle(e.dataTransfer.files);
   _CHRIS_INTERACTIVE_PLUGIN_.handleFiles(e.dataTransfer.files);
 };
 
-_CHRIS_INTERACTIVE_PLUGIN_.handleFiles = function (files) {
+_CHRIS_INTERACTIVE_PLUGIN_.handleStyle = function (files) {
   var filesFragment = document.createDocumentFragment();
   var domElements = [];
         
@@ -76,25 +88,43 @@ _CHRIS_INTERACTIVE_PLUGIN_.handleFiles = function (files) {
     // 100Mb limit
     //sudo vim /etc/php5/apache2/php.ini 
     //[toor@chris:x86_64-Linux]/var$>sudo service apache2 restart
-    // upload_max_filesize = 2M
-    // post_max_size = 2M
+    // upload_max_filesize = 100M
+    // post_max_size = 120M
     if(files[i].size < 838860800) {
       domElements = [
         document.createElement('div'),
         document.createElement('span')];
             
       domElements[1].appendChild(document.createTextNode(files[i].name + " (" + Math.round((files[i].size/1024*100000)/100000)+"K) "));
-        domElements[0].id = "item"+_CHRIS_INTERACTIVE_PLUGIN_.count;
-        domElements[0].appendChild(domElements[1]);
+      domElements[0].id = "item"+_CHRIS_INTERACTIVE_PLUGIN_.countStyle;
+      domElements[0].appendChild(domElements[1]);
             
       filesFragment.appendChild(domElements[0]);
+
+      // progress bar
+      var progressDomElements = [
+          document.createElement('div'),
+          document.createElement('div')];
+        
+      progressDomElements[0].className = "loader01";
+      progressDomElements[0].appendChild(progressDomElements[1]);
+      domElements[0].appendChild(progressDomElements[0]);
+
       _CHRIS_INTERACTIVE_PLUGIN_.dropListing.appendChild(filesFragment);
-            
-      _CHRIS_INTERACTIVE_PLUGIN_.processXHR(files[i], _CHRIS_INTERACTIVE_PLUGIN_.count);
-      _CHRIS_INTERACTIVE_PLUGIN_.count++;
+
+      _CHRIS_INTERACTIVE_PLUGIN_.countStyle++;
     } else {
         alert("The file you are trying to upload is too BIG (100Mb max)");
     }
+  }
+};
+
+_CHRIS_INTERACTIVE_PLUGIN_.handleFiles = function (files) {
+       
+  for(var i = 0, len = files.length; i < len; i++) {
+   
+      _CHRIS_INTERACTIVE_PLUGIN_.processXHR(files[i], _CHRIS_INTERACTIVE_PLUGIN_.count);
+      _CHRIS_INTERACTIVE_PLUGIN_.count++;
   }
 };
       
@@ -109,17 +139,8 @@ _CHRIS_INTERACTIVE_PLUGIN_.processXHR = function (file, index) {
   data.append(file.name, file);
 
   var container = document.getElementById("item"+index);
-  var loader = null;
+  var loader = document.getElementsByClassName("loader01");
   var fileUpload = xhr.upload;
-  var progressDomElements = [
-      document.createElement('div'),
-      document.createElement('div')];
-        
-  progressDomElements[0].className = "loader01";
-  progressDomElements[0].appendChild(progressDomElements[1]);
-        
-  container.appendChild(progressDomElements[0]);
-  loader = document.getElementsByClassName("loader01");
         
   fileUpload.addEventListener("progress", function(event) {
     if (event.lengthComputable) {
