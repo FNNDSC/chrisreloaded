4a5747ec24336f7b87c0f5ee735c244794359ddc
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2014-03-13 16:58:58 -0400

    Fix tempdir() permission issue. Simplify naming.

--- controller/_util.inc.php
+++ controller/_util.inc.php
@@ -195,11 +195,12 @@ function getConfiguration(){
   }
 
 # Create and return a unique temp dir in $dir with $prefix
-function tempdir(&$ssh, $dir = false, $prefix = '_chrisRun-') {
-    $tempfile=tempnam($dir, $prefix);
-    if (file_exists($tempfile)) { unlink($tempfile); }
-    $ssh->exec('mkdir -p '.$tempfile);
-    if (is_dir($tempfile)) { return $tempfile; }
+function tempdir(&$ssh, $dir = false, $prefix = '_chrisRun_') {
+    // $dirname  = uniqid($prefix, false);
+    $dirname  = $prefix;
+    $tempdir  = joinPaths($dir, $dirname);
+    $ssh->exec('mkdir -p '.$tempdir);
+    if (is_dir($tempdir)) { return $tempdir; }
 }
 
 # Simple debug console that writes $content to $outstem.
