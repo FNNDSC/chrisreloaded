5781a4420f25edabeb18153935eab14c57d51068
nicolasrannou <npm.rannou@gmail.com>
2014-05-07 16:55:51 -0400

    create the TIDO branch

---
+++ plugins/viewer/core/findDB.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ *            sSSs   .S    S.    .S_sSSs     .S    sSSs
+ *           d%%SP  .SS    SS.  .SS~YS%%b   .SS   d%%SP
+ *          d%S'    S%S    S%S  S%S   `S%b  S%S  d%S'
+ *          S%S     S%S    S%S  S%S    S%S  S%S  S%|
+ *          S&S     S%S SSSS%S  S%S    d* S  S&S  S&S
+ *          S&S     S&S  SSS&S  S&S   .S* S  S&S  Y&Ss
+ *          S&S     S&S    S&S  S&S_sdSSS   S&S  `S&&S
+ *          S&S     S&S    S&S  S&S~YSY%b   S&S    `S*S
+ *          S*b     S*S    S*S  S*S   `S%b  S*S     l*S
+ *          S*S.    S*S    S*S  S*S    S%S  S*S    .S*P
+ *           SSSbs  S*S    S*S  S*S    S&S  S*S  sSS*S
+ *            YSSP  SSS    S*S  S*S    SSS  S*S  YSS'
+ *                         SP   SP          SP
+ *                         Y    Y           Y
+ *
+ *                     R  E  L  O  A  D  E  D
+ *
+ * (c) 2012 Fetal-Neonatal Neuroimaging & Developmental Science Center
+ *                   Boston Children's Hospital
+ *
+ *              http://childrenshospital.org/FNNDSC/
+ *                        dev@babyMRI.org
+ *
+ */
+
+define('__CHRIS_ENTRY_POINT__', 666);
+
+// include the configuration
+require_once (dirname(dirname(dirname(dirname(__FILE__)))).'/config.inc.php');
+
+// convenience method to check if variable is set or not
+function is_set($variable, $value = '') {
+  return isset($variable)?$variable:$value;
+}
+
+// exact match on subjectname
+$feed_id = is_set($_POST['FEED_ID']);
+
+
+
+echo json_encode($feed_id);
+
+?>
--- plugins/viewer/widget/demo.js
+++ plugins/viewer/widget/demo.js
@@ -1,13 +1,153 @@
 // DEFINE NAMESPACE
 var _CHRIS_INTERACTIVE_PLUGIN_ = _CHRIS_INTERACTIVE_PLUGIN_ || {};
-// 1-OVERLOAD THE INIT METHOD
+
+// HELPERS
+_CHRIS_INTERACTIVE_PLUGIN_.getParam = function(parameter) {
+  if (typeof _CHRIS_INTERACTIVE_PLUGIN_._param[parameter] != 'undefined') {
+    if(_CHRIS_INTERACTIVE_PLUGIN_._param[parameter] == "")
+      return '1';
+
+    return _CHRIS_INTERACTIVE_PLUGIN_._param[parameter];
+  }
+  return "";
+};
+
+_CHRIS_INTERACTIVE_PLUGIN_.getInd = function(parameter) {
+  if (typeof _CHRIS_INTERACTIVE_PLUGIN_._param[parameter] != 'undefined') {
+    return _CHRIS_INTERACTIVE_PLUGIN_._param_ind[parameter];
+  }
+  return -1;
+};
+
+_CHRIS_INTERACTIVE_PLUGIN_.submitted = function(data) {
+  window.console.log('submitted');
+  var res = data.match(/\d+/g);
+  _CHRIS_INTERACTIVE_PLUGIN_.getDB(res[0]);
+
+}
+
 // when the html is loaded, we get the parameters from the plugin parameters
 _CHRIS_INTERACTIVE_PLUGIN_.init = function() {
+  window.console.log('init');
+  // if FEED_ID is not null, we just want to LOOK at the feed (it exists already)
+  // Call ajax here
+  // force it to start!
+  if(_CHRIS_INTERACTIVE_PLUGIN_.getParam("feedid") == ''){
+    _CHRIS_INTERACTIVE_PLUGIN_.force = true;
+    $("#plugin_submit").click();
+  }
+  else{
+    window.console.log(_CHRIS_INTERACTIVE_PLUGIN_.getParam("feedid"));
+    _CHRIS_INTERACTIVE_PLUGIN_.getDB(_CHRIS_INTERACTIVE_PLUGIN_.getParam("feedid"));
+  }
+}
+
+_CHRIS_INTERACTIVE_PLUGIN_.getDB = function(feedID){
+    window.console.log('getDB');
+  // ajax find matching directory!
+  jQuery.ajax({
+    type : "POST",
+    url : "plugins/viewer/core/findDB.php",
+    dataType : "text",
+    data : {
+      FEED_ID : feedID
+      },
+    success : function(data) {
+      window.console.log('YAY AJAX');
+      window.console.log(data);
+      _CHRIS_INTERACTIVE_PLUGIN_.startXTK(feedID);
+    }
+  });
+}
+
+_CHRIS_INTERACTIVE_PLUGIN_.togetherjsYO = function(feedID, threeD){
+
+  TogetherJSConfig_findRoom =  "chris" + feedID;
+
+window.console.log(TogetherJSConfig_findRoom);
+
+  TogetherJSConfig_on = {
+    ready: function(){_CHRIS_INTERACTIVE_PLUGIN_.togetherjsTestYO(threeD);}
+  };
+}
+
+_CHRIS_INTERACTIVE_PLUGIN_.onTouchStart = function(){
+  window.console.log('Touch Start');
+
+  _CHRIS_INTERACTIVE_PLUGIN_._updater = setInterval(function(){
+                var myJsonString = JSON.stringify(threeD.camera.view);
+                TogetherJS.send({type: "viewChanged", view:myJsonString});
+              }
+              , 150);
+}
+
+_CHRIS_INTERACTIVE_PLUGIN_.onTouchEnd = function(){
+
+  window.console.log('TouchEnd');
+  clearInterval(_CHRIS_INTERACTIVE_PLUGIN_._updater);
+  
+}
+
+_CHRIS_INTERACTIVE_PLUGIN_.togetherjsTestYO = function(threeD){
+  window.console.log('I am READY!');
+
+  // to be stopped when view closed
+  threeD.interactor.onTouchStart = threeD.interactor.onMouseDown = _CHRIS_INTERACTIVE_PLUGIN_.onTouchStart;
+  threeD.interactor.onTouchEnd = threeD.interactor.onMouseUp = _CHRIS_INTERACTIVE_PLUGIN_.onTouchEnd;
+ // ren3d.interactor.onMouseWheel = function(e) {
+//       setInterval(function(threeD){
+//         var myJsonString = JSON.stringify(threeD.camera.view);
+// TogetherJS.send({type: "viewChanged", view:myJsonString});
+// },1000,threeD);
+
+      TogetherJS.hub.on("viewChanged", function (msg) {
+        if (! msg.sameUrl) {
+          return;
+        }
+
+        window.console.log(msg);
+        var obj = JSON.parse(msg.view);
+        var arr = $.map(obj, function(el) { return el; });
+        threeD.camera.view = new Float32Array(arr);
+      });
+
+  //TogetherJS.send({type: "visibilityChange", isVisible: isVisible, element: element});
+//TogetherJS.hub.on("visibilityChange", function (msg) {
+//   var elementFinder = TogetherJS.require("elementFinder");
+//   // If the element can't be found this will throw an exception:
+//   var element = elementFinder.findElement(msg.element);
+//   MyApp.changeVisibility(element, msg.isVisible);
+// });
+}
+
+// _CHRIS_INTERACTIVE_PLUGIN_.RTpushCamera = function(renderer) {
+
+//   var _current_view = Array.apply([], eval(renderer).camera.view);
+
+//   if ( !arraysEqual(_current_view, RT._old_view) ) {
+
+//     RT._link.trigger('client-camera-sync', {
+//       'target' : renderer,
+//       'value' : _current_view
+//     });
+
+//     RT._old_view = _current_view;
+
+//   }
+
+// };
+
+
+_CHRIS_INTERACTIVE_PLUGIN_.startXTK = function(feedID){
 
   // try to create and initialize a 3D render
   threeD = new X.renderer3D();
   threeD.container = '33d';
   threeD.init();
+
+  // configure room name based on feed ID
+  window.console.log("XTK FEEDID: " + feedID);
+  _CHRIS_INTERACTIVE_PLUGIN_.togetherjsYO(feedID, threeD);
   
   //
   // create the 2D renderers
@@ -27,7 +167,6 @@ _CHRIS_INTERACTIVE_PLUGIN_.init = function() {
   sliceZ.orientation = 'Z';
   sliceZ.init();
   
-
   //
   // THE VOLUME DATA
   //
@@ -99,6 +238,5 @@ _CHRIS_INTERACTIVE_PLUGIN_.init = function() {
     volumegui.open();
 
   };
-
 
 }
