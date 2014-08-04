8636aa6b95fc195cc939c24866df3bb5f0fb5480
Jorge <jbernal0019@yahoo.es>
2014-08-04 17:50:27 -0400

    implemented UserC::setupDir()

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -34,6 +34,7 @@ require_once (dirname(dirname(__FILE__)).'/config.inc.php');
 require_once (joinPaths(CHRIS_CONTROLLER_FOLDER, 'mapper.class.php'));
 
 require_once ('Net/SSH2.php');
+require_once ('Net/SCP.php');
 
 // interface
 interface UserControllerInterface
@@ -73,7 +74,7 @@ class UserC implements UserControllerInterface {
     }
     return self::$groupId;
   }
-  
+
   /**
    * Get user id from username. Returns -1 if no match.
    * @return int
@@ -181,45 +182,66 @@ class UserC implements UserControllerInterface {
    */
   static public function setupDir($username, &$ssh) {
 
-    $user_path = joinPaths(CHRIS_USERS, $username);
+    echo 'I was called';
+    $userHomeDir = $ssh->exec('pwd');
+    $groupId = self::getGroupId();
+    $server_user_path = joinPaths(CHRIS_USERS, $username);
 
-    // create home directory (if does't exist)
-    if(!file_exists($user_path)){
-
-      $ssh->exec("mkdir $user_path;chmod 775 $user_path;");
-
+    //create users' home directory (if doesn't exist)
+    if(!file_exists($userHomeDir)){
+      mkdir($userHomeDir, 0700, true);
+      chown($userHomeDir, $username);
+      chgrp($userHomeDir, $groupId);
     }
 
-    // create config directory  (if does't exist)
-    $user_config_path = joinPaths($user_path, CHRIS_USERS_CONFIG_DIR);
+    // create user directory within Chris (if does't exist)
+    if(!file_exists($server_user_path)){
+
+      mkdir($server_user_path, 0775);
+      chown($server_user_path, $username);
+      chgrp($server_user_path, $groupId);
+    //$ssh->exec("mkdir $user_path;chmod 775 $user_path;");
+    }
+
+    // create users' config directory  (if does't exist)
+    $user_config_path = joinPaths($server_user_path, CHRIS_USERS_CONFIG_DIR);
     if(!file_exists($user_config_path)){
 
-      $ssh->exec("mkdir $user_config_path;chmod 775 $user_config_path;");
-
+      mkdir($user_config_path, 0775);
+      chown($user_config_path, $username);
+      chgrp($user_config_path, $groupId);
+      //$ssh->exec("mkdir $user_config_path;chmod 775 $user_config_path;");
     }
 
     // add default configuration file  (if does't exist)
     $user_config_file = joinPaths($user_config_path, CHRIS_USERS_CONFIG_FILE);
     $chris_config_file = joinPaths(CHRIS_SRC, CHRIS_USERS_CONFIG_FILE);
     if(!file_exists($user_config_file)){
-
-      $ssh->exec("cp  $chris_config_file $user_config_file;chmod 660 $user_config_file;");
-
+      copy($chris_config_file, $user_config_file);
+      chown($user_config_file, $username);
+      chgrp($user_config_file, $groupId);
+      //$ssh->exec("cp  $chris_config_file $user_config_file;chmod 660 $user_config_file;");
     }
 
     // generate ssh key for passwordless ssh  (if does't exist)
-    $user_key_file = joinPaths($user_config_path, CHRIS_USERS_CONFIG_SSHKEY);
+    $keyDir = joinPaths($userHomeDir, '.ssh');
+    $user_key_file = joinPaths($keyDir, CHRIS_USERS_CONFIG_SSHKEY);
     if(!file_exists($user_key_file)){
-
-      $ssh->exec('ssh-keygen -t rsa -N "" -f '.$user_key_file.';');
-
+      if (!remoteDirExists($ssh, $keyDir)) {
+        $ssh->exec('mkdir -p '.$keyDir.';');
+      }
+      $ssh->exec('ssh-keygen -t rsa -N "" -f '.$cluster_user_key_file.';');
     }
 
     // id_rsa.pub to user's authorized keys if needed
-    $ssh->exec('/bin/bash -c "mkdir -p ~/.ssh; (cat ~/.ssh/authorized_keys | grep \"$(cat '.$user_key_file.'.pub)\") || (cat '.$user_key_file.'.pub >> ~/.ssh/authorized_keys;ssh-add;)"');
-
+    $ssh->exec('/bin/bash -c "(cat ~/.ssh/authorized_keys | grep \"$(cat '.$user_key_file.'.pub)\") || (cat '.$user_key_file.'.pub >> ~/.ssh/authorized_keys;ssh-add;)"');
     // make sure the permissions are correct to allow ssh with id_rsa
     $ssh->exec('chmod go-w ~/.ssh;chmod 600 ~/.ssh/authorized_keys;chown `whoami` ~/.ssh/authorized_keys;');
+    //Compress .ssh dir
+    $ssh->exec('tar -zcf ssh.tar.gz ~/.ssh;');
+    $scp = new Net_SCP($ssh);
+    $scp->get('~/ssh.tar.gz', $userHomeDir.'/ssh.tar.gz');
+    $output = shell_exec('tar -zxf '.$userHomeDir.'/ssh.tar.gz');
 
  }
 
