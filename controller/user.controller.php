5e44d34b550430afdb9fd9d3d90fe8af39926b65
Jorge <jbernal0019@yahoo.es>
2014-10-16 15:09:19 -0400

    the user config file is now created with 664 permissions

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -245,7 +245,7 @@ class UserC implements UserControllerInterface {
     $user_config_file = joinPaths($user_config_path, CHRIS_USERS_CONFIG_FILE);
     $chris_config_file = joinPaths(CHRIS_SRC, CHRIS_USERS_CONFIG_FILE);
     if(!file_exists($user_config_file)){
-      $ssh->exec('cp  '.$chris_config_file.' '.$user_config_file. ';chmod 660  '.$user_config_file.';');
+      $ssh->exec('cp  '.$chris_config_file.' '.$user_config_file. ';chmod 664  '.$user_config_file.';');
     }
   }
 
