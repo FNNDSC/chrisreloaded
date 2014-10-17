50416b386e7472dc765c00d10b7308150469f27d
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2014-10-17 17:11:28 -0400

    Make more user friendly.

--- tools/checkDB.bash
+++ tools/checkDB.bash
@@ -1,8 +1,57 @@
-#! /bin/bash
+#!/bin/bash
 
 dbUser="chris"
-dbName="chrisdev"
-usersDirectory="/neuro/users/chris/dev/users"
+dbName="chris"
+usersDirectory="/home/chris/users"
+
+G_SYNOPSIS="
+
+ NAME
+
+       checkDB.bash
+
+ SYNOPSIS
+
+       checkDB.bash -h <usersDirectory> -n <dbName> -u <dbUser>
+
+ DESCRIPTION
+
+        'checkDB.bash' is a simple script that checks and fixes inconsistencies
+	between a user's feed as recorded in the ChRIS database and the feed
+	on the filesystem.
+
+	If inconsistencies are found, the script will present the option of 
+
+		[1] - Remove the entry from the DB if there is no corresponding
+		      feed on the filesystem.
+		[2] - Create a directory again on the filesystem. Note that this
+		      feed directory will of course be empty.
+		[3] - Ignore.
+
+ ARGS
+
+	-h <usersDirectory> (defaults to: $usersDirectory)
+	The 'home' directory within the ChRIS tree. This directory contains the
+	feeds of each user, organized by user name.
+
+	-n <dbName> (defaults to $dbName)
+	The name of the data base instance running on the system.
+
+	-u <dbUser> (defaults to $dbUser)
+	The ChRIS user to check.
+  
+ PRECONDITIONS
+
+	o The script user must know the database MySQL password.
+        o A ChRIS DB that has been instantiated.
+	o User's feed directories.
+
+ POSTCONDITIONS
+
+	o DB made consistent to the filesystem.
+
+"
+
 
 sqlCommand="SELECT user.username,feed.plugin,feed.name,feed.id FROM user JOIN feed ON user.id=feed.user_id;"
 sqlDeleteFeedCommand="DELETE FROM feed WHERE feed.id="
@@ -10,7 +59,24 @@ sqlDeleteFeedDataCommand="DELETE FROM feed_data WHERE feed_data.feed_id="
 sqlDeleteFeedTagCommand="DELETE FROM feed_tag WHERE feed_tag.feed_id="
 sqlDeleteFeedMetaCommand="DELETE FROM meta WHERE meta.target_type=\"feed\" AND meta.target_id="
 
+while getopts u:h:n: option ; do
+        case "$option"
+        in
+                u)      dbUser=$OPTARG			;;
+                n)      dbName=$OPTARG			;;
+                h)      usersDirectory="$OPTARG"        ;;
+                \?)     echo "$G_SYNOPSIS"
+                        exit 0;;
+        esac
+done
+
 re='^[0-9]+$'
+
+printf "Performing DB/filesystem consistency check for:\n"
+printf "%20s%30s\n" "user directory:" "[ $usersDirectory ]"
+printf "%20s%30s\n" "user:" "[ $dbUser ]"
+printf "%20s%30s\n" "name:" "[ $dbName ]"
+
 
 while read a b c d
 do
@@ -23,7 +89,7 @@ do
 	then
 		echo "$feedDirectory exists!"
 	else
-		echo "$feedDirectory DOESNT exists!"
+		echo "$feedDirectory DOES NOT exist!"
 		while true; do
           		read -p "Do you wish to remove it from the DB [1] or add it to the file system [2] or ignore [3]?" yn </dev/tty
 			case $yn in
