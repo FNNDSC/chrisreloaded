2b5a2e92527f861f8511844541ce5ee02581723e
Nicolas Rannou <huko.rannou@gmail.com>
2014-10-20 05:47:23 -0400

    work on anonymization in the launcher

--- controller/launcher.php
+++ controller/launcher.php
@@ -473,23 +473,31 @@ else
 
     //ANONYMIZATION
     if (ANONYMIZE_DICOM) {
-      //$dir_array contains the list of all subdirectories in the tree with root $chrisInput_path
-      $dir_iter = new RecursiveDirectoryIterator($chrisInput_path, RecursiveDirectoryIterator::SKIP_DOTS);
-      $iter = new RecursiveIteratorIterator($dir_iter, RecursiveIteratorIterator::SELF_FIRST);
-      $dir_array = array($chrisInput_path);
-      foreach ($iter as $dir => $dirObj) {
-        if ($dirObj->isDir()) {
-          $dir_array[] = $dir;
-        }
-      }
-      //for each subdirectory in the tree find out if it contains dicom files and if so then run anonymization
-      //the output goes to the same directory overwriting the previous dicom file
-      foreach ($dir_array as $dir) {
-        $dicomFiles = glob($dir.'/*.dcm');
-        if (count($dicomFiles)) {
-          $sshLocal->exec('PATH='.joinPaths(CHRIS_HOME, 'bin').':'.joinPaths(CHRIS_SRC, '../scripts').':$PATH; dcmanon_meta.bash -P -O ' . $dir . ' -D ' . $dir);
-        }
-      }
+      $anonfile = joinPaths($job_path_output, 'chris.anon');
+      // PHP headers
+      $anonCmd = '#!/usr/bin/php
+	          <?php
+	          $dir_iter = new RecursiveDirectoryIterator('.$chrisInput_path.', RecursiveDirectoryIterator::SKIP_DOTS);
+                  $iter = new RecursiveIteratorIterator($dir_iter, RecursiveIteratorIterator::SELF_FIRST);
+                  $dir_array = array('.$chrisInput_path.');
+                  foreach ($iter as $dir => $dirObj) {
+                      if ($dirObj->isDir()) {
+                          $dir_array[] = $dir;
+                      }
+                  }
+                  
+                  //for each subdirectory in the tree find out if it contains dicom files and if so then run anonymization
+                  //the output goes to the same directory overwriting the previous dicom file
+                  foreach ($dir_array as $dir) {
+                      $dicomFiles = glob($dir."/*.dcm");
+                      if (count($dicomFiles)) {
+                          shell_exec("PATH=".joinPaths('.CHRIS_HOME.', "bin").":".joinPaths('.CHRIS_SRC.', "../scripts").":$PATH; dcmanon_meta.bash -P -O " . $dir . " -D " . $dir);
+                      }
+                  }
+                  ?>';
+      
+      $sshLocal->exec('echo '.$anonCmd.' >  '.$anonfile);   
+      $sshLocal->exec('chmod 755 '.$anonfile);   
     }
 
     //
